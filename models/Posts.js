772a5781ed04b2b70372f67ac317430f5de1e7cb
remy-mugisha <mugisharemy2019@gmail.com>
2023-02-09 12:43:06 +0200

    post

---
+++ models/Posts.js
@@ -0,0 +1,18 @@
+const mongoose=require('mongoose');
+
+const PostSchema=new mongoose.Schema({
+    title:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    desc:{
+        type:String,
+    },
+    image: {
+        type: String,
+    },
+},{
+    timestamps:true}
+);
+module.exports=mongoose.model('Post',PostSchema);
